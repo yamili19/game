ad73fd0b8288ec14a167ef36530a9fae11e41df5
yamili19 <57031600+yamili19@users.noreply.github.com>
2025-03-24 15:53:57 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from flask import Flask, render_template
-
+import os
 app = Flask(__name__)
 
 @app.route('/')
@@ -22,5 +22,6 @@ def buscaminas():
 def pinball():
     return render_template('pinball.html')
 
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', debug=True)
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))  # <-- Puerto dinámico para Render
+    app.run(host="0.0.0.0", port=port)  # <-- Acepta conexiones externas
